938de92a48c2fe7aa7f9a53953a2d317a69b33ee
thpigs <leinadgnach@gmail.com>
2025-03-03 15:49:06 -0500

    Initial commit of clean XCode project

---
+++ nurse-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nurse-app
+//
+//  Created by Chang, Daniel Soobin on 3/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nurse-app/nurse_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nurse_appApp.swift
+//  nurse-app
+//
+//  Created by Chang, Daniel Soobin on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct nurse_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
